374fff7d479521fa5ff633df3413a061290ed773
Sjaak Luthart <mail@sjaakluthart.nl>
2016-03-20 18:43:57 +0100

    added links

--- src/app/bacon.jsx
+++ src/app/bacon.jsx
@@ -12,17 +12,16 @@ const Bacon = React.createClass({
 
   componentDidMount() {
     window.setTimeout(() => {
-      console.log('timed out');
       this.setState({
         bacon: true
       })
-    }, 2000);
+    }, 3000);
   },
 
   render() {
     return (
       <section className="bacon">
-        {this.state.bacon ? <div><img className="strip" src="assets/bacon.jpg" /><img className="strip" src="assets/bacon.jpg" /><img className="strip" src="assets/bacon.jpg" /><p>bacon</p></div> : <div><img className="knife" src="assets/mes.jpg" /><img className="hook" src="assets/haak.jpg" /></div>}
+        {this.state.bacon ? <div><img className="strip" src="assets/bacon.jpg" /><p>bacon</p></div> : <div><img className="knife" src="assets/mes.jpg" /><img className="hook" src="assets/haak.jpg" /></div>}
 
       </section>
     );
--- src/app/start.jsx
+++ src/app/start.jsx
@@ -16,12 +16,20 @@ const Start = React.createClass({
     });
   },
 
+  showMud() {
+    routie('#mud');
+  },
+
+  showBacon() {
+    routie('#bacon');
+  },
+
   render() {
     return (
       <section className="start">
         {this.state.started ? <p>Maak uw keuze</p> : <p><span className="start1">Dit is</span><span className="start2"> Frankie,</span></p>}
         <img src="assets/frankie.jpg" />
-        {this.state.started ? <p><span className="left">modder</span><span className="right">bacon</span></p> : <p><span className="start3">Wat gaat Frankie</span><span className="start4"> beleven?</span></p>}
+        {this.state.started ? <p><span onClick={this.showMud} className="left">modder</span><span  onClick={this.showBacon} className="right">bacon</span></p> : <p><span className="start3">Wat gaat Frankie</span><span className="start4"> beleven?</span></p>}
         {!this.state.started ? <p onClick={this.start}>start</p> : null}
       </section>
     );
